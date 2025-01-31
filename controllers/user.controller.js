1a854815230ae4c6e656bb263f5b50bfce45279a
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2025-01-31 14:44:43 +0200

    user issue

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -50,7 +50,8 @@ const updateUserDetails = async (req, res) => {
       },
       data: req.body,
     });
-    res.status(200).json(user);
+    const { username,  whatsapp , email } = user;
+    res.status(200).json({ username, whatsapp, email });
   } catch (error) {
     res.status(500).json({ error: "Failed to update user details." });
   }
